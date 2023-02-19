3a77ca52c20022a4ef7fe7592d92b714a3c46db7
Iryna Levko <iryna.b.levko@gmail.com>
2023-02-19 13:03:03 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports =  function reverse (n) {
+    let numA = Math.abs(n);
+    let numB = numA.toString();
+    let numC = numB.split('').reverse().join('');
+    let numResult = parseInt(numC , 10);
+    return(numResult)
 }
